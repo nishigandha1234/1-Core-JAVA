a13104508c8f788333cda7dcae9b9632ccfaf393
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-13 22:43:47 +0530

    Rotate COde Added

--- 3-Looping_Statements/2-For_Loop/RotateKTimes.java
+++ 3-Looping_Statements/2-For_Loop/RotateKTimes.java
@@ -7,17 +7,46 @@ class RotateKTimes
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Number : ");
 		int num = sc.nextInt(); //123456
+		System.out.println("Enter Value of K : ");
+		int k = sc.nextInt(); //2
 		int rev = 0 ;
 		int temp = num;
 		
 		while(temp != 0)
 		{
-			rev = rev*10 + (temp%10);
+			rev = rev*10+(temp%10);
 			temp/=10;
 		}
+		System.out.println(rev);
 		
-		//find value of k & pow it
+		//find value of k & pow it k=2 
+		//	i=1		   2<=2
+		int pow=1;
+		for(int i = 1 ; i <= k ; i++)
+		{
+			pow*=10; 
+		}
+		//System.out.println(pow); //100
 		
+		int res = rev%pow; //654321%100 == 21
+		int revLast = 0;
+		while(res!=0) 
+		{
+			int dig = res%10;
+			revLast = revLast*10 + dig;
+			res/=10; 
+		}
+		//System.out.println(revLast);
 		
+		int revFirst = 0;
+		int ans = rev/pow;
+		while(ans != 0)
+		{
+			int digit = ans%10;
+			revFirst = revFirst*10 + digit;
+			ans/=10;
+		}
+		//System.out.println(revFirst);
+		System.out.println(revFirst+""+revLast);
 	}
 }
