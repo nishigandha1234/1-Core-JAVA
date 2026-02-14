b4a24768d78c6dc83413092f86849f68bb624530
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-14 11:08:14 +0530

    New Programs added of Series

---
+++ 7-NumberSeriesPrograms/Series55.java
@@ -0,0 +1,12 @@
+// 5  3  10  6  15  9  20  12  25  15  30  18  35  21  40
+
+class Series55
+{
+	public static void main(String x[])
+	{
+		for(int i =1 ; i<= 8;i++)
+		{
+			System.out.print((5*i)+" "+(3*i)+" ");
+		}
+	}
+}
