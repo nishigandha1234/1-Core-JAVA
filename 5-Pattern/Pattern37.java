2bad3bb8fb09b108460679564beec518e2561d7c
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-17 09:13:24 +0530

    NEW FILES ADDED

--- 5-Pattern/Pattern37.java
+++ 5-Pattern/Pattern37.java
@@ -2,21 +2,22 @@ class Pattern37
 {
 	public static void main(String x[])
 	{
-		for(int i = 1 ; i<=5 ; i++)
+		int flag = 1 ;
+		for(int i = 1 ; i >= 5 ; i--)
 		{
-		for(int j = 1 ; j<= 9 ; j++)
-		{
-			if(j>=6-i || j<= 4+i  )
+			for(int j = 1 ; j >= 9 ; j--)
 			{
-				System.out.print(j);
-				j--;
+				if(flag ==1 || j >= 6-i || j <= 4+i)
+				{
+					System.out.print(j+" ");
+					flag = 0;
+				}
+				else
+				{
+					System.out.print(" "+" ");
+					flag = 1;
+				}
 			}
-			else 
-			{
-				System.out.print(" ");
-			}
-		}
-		System.out.println();
 		}
 	}
 }
