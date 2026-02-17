d9b8f8d42d2a1bc9dcdee0c163a849d832990380
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-17 09:12:14 +0530

    NEW FILES ADDED

--- 5-Pattern/Pattern39.java
+++ 5-Pattern/Pattern39.java
@@ -2,20 +2,20 @@ class Pattern39
 {
 	public static void main(String x[])
 	{
-		for(int i = 5 ; i >=1 ; i--)
+		for(int i = 5 ; i >= 1 ; i--)
 		{
 			for(int j = 5 ; j >= 1 ; j--)
 			{
-				if(j<=6-i)
+				if(j>=6-i)
 				{
-					if(i==1 || i ==3 || i ==5)
-						System.out.print(j);
+					if(j==1 || j ==3 || j ==5)
+						System.out.print(i+" ");
 					else
-						System.out.print("*");
+						System.out.print("*"+" ");
 				}
 				else
 				{
-					System.out.print(" ");
+					System.out.print("  ");
 				}
 			}				
 			System.out.println();
