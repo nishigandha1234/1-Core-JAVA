40ed7f4425a4a4d8e4fbb593aad77d986879276f
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-17 09:59:20 +0530

    NEW FILES ADDED

--- 5-Pattern/Pattern40.java
+++ 5-Pattern/Pattern40.java
@@ -2,19 +2,22 @@ class Pattern40
 {
 	public static void main(String x[])
 	{
+		
 		for(int i = 1 ; i <= 5 ; i++)
 		{
 			int num = 1;
 			for(int j = 1 ; j <= 9 ; j++)
 			{
-				if(j <= 6-i && j >= 4+i)
+				if(j >= 6-i && j <= 4+i)
 				{
-					System.out.print(num++ +" ");
+					System.out.print(num+" ");
+					num= j>=5?--num:++num;
 				}
 				else
 				{
-					System.out.print(" "+" ");
+					System.out.print("  ");
 				}
+				
 			}
 			System.out.println();
 		}
