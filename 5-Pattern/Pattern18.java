73bcf0191aa3a5d069901c87beb5564a152f8d67
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-17 09:29:19 +0530

    NEW FILES ADDED

--- 5-Pattern/Pattern18.java
+++ 5-Pattern/Pattern18.java
@@ -3,11 +3,11 @@ class Pattern18
 	public static void main(String x[])
 	{
 		int flag = 1 ;
-		for(int i = 1 ; i >= 5 ; i--)
+		for(int i = 5 ; i >= 1 ; i--)
 		{
-			for(int j = 1 ; j >= 9 ; j--)
+			for(int j = 9 ; j >= 1 ; j--)
 			{
-				if(flag ==1 || j >= 6-i || j <= 4+i)
+				if(flag ==1 && (j >= 6-i && j <= 4+i))
 				{
 					System.out.print("*"+" ");
 					flag = 0;
@@ -18,6 +18,7 @@ class Pattern18
 					flag = 1;
 				}
 			}
+			System.out.println();
 		}
 	}
 }
--- 5-Pattern/Pattern37.java
+++ 5-Pattern/Pattern37.java
@@ -2,22 +2,21 @@ class Pattern37
 {
 	public static void main(String x[])
 	{
-		int flag = 1 ;
-		for(int i = 1 ; i >= 5 ; i--)
+		for(int i = 1 ; i <= 5 ; i++)
 		{
-			for(int j = 1 ; j >= 9 ; j--)
+			int num = 1;
+			for(int j = 1 ; j <= 9 ; j++)
 			{
-				if(flag ==1 || j >= 6-i || j <= 4+i)
+				if(j <= 10-i && j >= i)
 				{
-					System.out.print(j+" ");
-					flag = 0;
+					System.out.print(num++ +" ");
 				}
 				else
 				{
 					System.out.print(" "+" ");
-					flag = 1;
 				}
 			}
+			System.out.println();
 		}
 	}
 }
