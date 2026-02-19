c4329b2778e8a30fd6f6ac8cadbca4efd687bd80
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-19 12:35:42 +0530

    New Files

--- 5-Pattern/Pattern50.java
+++ 5-Pattern/Pattern50.java
@@ -2,11 +2,11 @@ class Pattern50
 {
 	public static void main(String x[])
 	{
-	for(int i = 1 ; i <= 4 ; i++)
+	for(int i = 1 ; i <= 7 ; i++)
 	{
-		for(int j = 4 ; j >= 1 ; j--)
+		for(int j = 1 ; j <= 4 ; j++)
 		{
-			if(j >= 5-i)
+			if(j >= 5-i && j<= 3+i)
 			{
 				System.out.print(i+" ");
 			}
