c5fe9986756b68c8a5c85d0908af2271f4fa5d00
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-19 12:45:12 +0530

    New Files

--- 5-Pattern/Pattern51.java
+++ 5-Pattern/Pattern51.java
@@ -2,8 +2,19 @@ class Pattern51
 {
 	public static void main(String x[])
 	{
-		
-	
-	
+		for(int i = 1 ; i <= 5 ; i++)
+		{
+			for(int j = 1 ; j <= 5 ; j++)
+			{
+				if(j == 6-i)
+				{
+					System.out.print("#"+" ");
+				}
+				else{
+					System.out.print(j+" ");
+				}
+			}
+			System.out.println();
+		}
 	}
 }
