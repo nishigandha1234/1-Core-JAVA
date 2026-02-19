1dcec1727cca27b129366899383219ac60215e45
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-19 13:28:14 +0530

    New Files

--- 5-Pattern/Pattern56.java
+++ 5-Pattern/Pattern56.java
@@ -2,11 +2,11 @@ class Pattern56
 {
 	public static void main(String x[])
 	{
-		for(int i = 1 ; i<=5 ; i++)
+		for(int i = 1 ; i <= 5 ; i++)
 		{
 			for(int j = 1 ;  j <= 9 ; j++)
 			{
-				if(j >= 5-i)
+				if(j >= 5-i && j<= 3+i)
 				{
 					System.out.print(i+" ");
 				}
