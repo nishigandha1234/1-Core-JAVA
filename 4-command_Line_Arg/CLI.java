8af8500221c27c175c8c443b0c9e26b39ddef4e6
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-25 22:26:46 +0530

    New Files Added

---
+++ 4-command_Line_Arg/CLI.java
@@ -0,0 +1,13 @@
+class CLI
+{
+	public static void main(String x[])
+	{
+		int a = Integer.parseInt(x[0]);
+		
+		if(a%2 == 0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+		
+	}
+}
