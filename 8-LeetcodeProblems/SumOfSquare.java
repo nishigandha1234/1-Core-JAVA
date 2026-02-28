d29a9de61863a43fc06181057a65728455fcc103
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-28 15:31:04 +0530

    New Arrays Folder Added

--- 8-LeetcodeProblems/SumOfSquare.java
+++ 8-LeetcodeProblems/SumOfSquare.java
@@ -2,8 +2,7 @@
 //Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
 
 class SumOfSquare 
-{
-	
+{	
 	public static void main(String x[])
 	{
 		System.out.println(judgeSquareSum(5));
