1e5db94dd8fa8d2a45dd194fbe0755cc21289494
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-28 15:28:20 +0530

    New Arrays Folder Added

---
+++ 8-LeetcodeProblems/SumOfSquare.java
@@ -0,0 +1,31 @@
+//633. Sum of Square Numbers
+//Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+
+class SumOfSquare {
+    public boolean judgeSquareSum(int c) {
+        long i = 0 ;
+        long j =  (long) Math.sqrt(c);
+
+        int left = 0;
+        int right = 0;
+
+        while(left <= right)
+        {
+            int sum = left*left + right*right;
+
+            if(sum == c)
+            {
+                return true;
+            }
+            else if(sum < c)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+            return false;
+        }
+    }
+}
