3abaa7117c518a07419880d32786452a1a2397c6
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-28 12:44:16 +0530

    New Arrays Folder Added

--- 9-DSA/1-Array/FindSecondMax.java
+++ 9-DSA/1-Array/FindSecondMax.java
@@ -2,4 +2,29 @@ import java.util.Scanner;
 
 public class FindSecondMax
 {
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(Systen.in);
+		System.out.println("Enter the Values in Arrays : ");
+		int[] ar = new int[6];
+		for(int i = 0 ; i < ar.length ; i++)
+		{
+			ar[i] = sc.nextInt();
+		}
+		int max = ar[0];
+		int secMax = Integer.MIN_LENGTH;
+		for(int i = 1 ; i< ar.length ; i++)
+		{
+			if(ar[i] > max)
+			{
+				secMax = max;
+				max= ar[i];
+			}
+			if(ar[i] > secMax && ar[i] < max)
+			{
+				secMax = ar[i];
+			}
+		}
+		System.out.println(secMax);
+	}
 }
