b69abf7c5f6f313976afcee2ef8b1ecb1a2fe00c
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-28 15:53:57 +0530

    New Arrays Folder Added

---
+++ 8-LeetcodeProblems/MIssingNumber.java
@@ -0,0 +1,22 @@
+//268. Missing Number
+//Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+import java.util.Scanner;
+class MissingNumber
+{
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Values : ");
+		int[] nums;
+		for(int i = 0 ; i < nums.length ; i++)
+		{
+			int[] nums = sc.nextInt();
+		}
+		System.out.println(missingNumber(int[] nums));
+	}
+	public int missingNumber(int[] nums) 
+	{
+		
+    }
+}
