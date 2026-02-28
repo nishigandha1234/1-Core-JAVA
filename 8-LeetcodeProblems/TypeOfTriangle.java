85e1a39080e3bf2f8df169f929ddf040e5ffa52d
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-28 15:01:22 +0530

    New Arrays Folder Added

---
+++ 8-LeetcodeProblems/TypeOfTriangle.java
@@ -0,0 +1,14 @@
+//3024
+
+class TypeOfTriangle {
+    public String triangleType(int[] nums) {
+        if(nums[0] == nums[1] && nums[1] == nums[2] && nums[0] == nums[2])
+            return "equilateral";
+        else if(nums[0] == nums[1] || nums[1] == nums[2] || nums[0]==nums[2]) 
+            return "isosceles";
+        else if(nums[0] != nums[1] || nums[1] != nums[2] || nums[0]!=nums[2]) 
+            return "scalene";
+        else
+            return "none";
+    }
+}
