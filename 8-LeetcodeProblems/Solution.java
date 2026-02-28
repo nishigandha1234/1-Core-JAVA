2f049b9f2a7fc6adbbbe1b3ec52204517cfd7d42
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-02-28 13:35:00 +0530

    New Arrays Folder Added

---
+++ 8-LeetcodeProblems/Solution.java
@@ -0,0 +1,15 @@
+//leetcode - 258
+
+class Solution {
+    public int addDigits(int num) {
+        int sum = 0;
+        int temp = num;
+        while(emp > 0 && temp <= 9)
+        {
+            int rem = temp % 10;
+            sum += rem;
+            temp /= 10;
+        }
+        System.out.println(temp);
+    }
+}
