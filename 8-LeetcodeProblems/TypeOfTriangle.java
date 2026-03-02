017e84139876a52ca208fab52be3a64566dc5279
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-02 20:26:29 +0530

    New Leetcode Problems added

--- 8-LeetcodeProblems/TypeOfTriangle.java
+++ 8-LeetcodeProblems/TypeOfTriangle.java
@@ -2,8 +2,7 @@
 
 import java.util.Arrays;
 
-class TypeOfTriangle 
-{
+class TypeOfTriangle {
     public String triangleType(int[] nums) {
         Arrays.sort(nums);   // Sort to make triangle check easy
         
