208d6166497462c78ac8c4824f7f4cbaf1bfb1d0
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-03 11:25:53 +0530

    New Leetcode Problems added

--- 8-LeetcodeProblems/SumOfSquare.java
+++ 8-LeetcodeProblems/SumOfSquare.java
@@ -1,6 +1,5 @@
 //633. Sum of Square Numbers
 //Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c. 
-
 class SumOfSquare 
 {	
 	public static void main(String x[])
