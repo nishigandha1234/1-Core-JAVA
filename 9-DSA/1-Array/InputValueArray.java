ed81b162d4d97c63911e8198c7523ba1b11d52ff
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-03 11:33:19 +0530

    New Leetcode Problems added

---
+++ 9-DSA/1-Array/InputValueArray.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class InputValueArray
+{
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number : ");
+		int ar[] = new int[5];
+		
+		for(int i = 0 ; i < ar.length ; i++)
+		{
+			int ar[i] = sc.nextInt();
+		}
+		
+		for(int i = 0 ; i < ar.length ; i++)
+		{
+			System.out.println(ar[i]);
+		}
+	
+	}
+}
