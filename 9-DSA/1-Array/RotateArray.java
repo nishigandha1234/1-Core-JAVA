2f4502a291003f61631bc85bb9801047b641095c
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-04 12:53:59 +0530

    New Array Programs Added Successfully...!

--- 9-DSA/1-Array/RotateArray.java
+++ 9-DSA/1-Array/RotateArray.java
@@ -6,7 +6,17 @@ class RotateArray
 	{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Values in Array : ");
-		int a [] = new int[5];
-		
+		int a[] = new int[5];
+		int[] ans = new int[a.length];
+		int k = a.length;
+		for(int i = 0 ; i < a.length ; i++)
+		{
+			ans[i] = a[k]; 
+			k--;
+		}
+		for(int i = 0 ; i < ans.length ; i++)
+		{
+			System.out.print(ans[i] + " ");
+		}
 	}
 }
