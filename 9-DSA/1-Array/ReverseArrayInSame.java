52eee705f2c7222a7be8da220eda8550d9f11845
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-04 12:58:24 +0530

    New Array Programs Added Successfully...!

---
+++ 9-DSA/1-Array/ReverseArrayInSame.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class ReverseArrayInSame
+{
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Values in Array : ");
+		int a[] = new int[5];
+		int[] ans = new int[a.length];
+		int k = a.length-1;
+		for(int i = 0 ; i < a.length ; i++)
+		{
+			a[i] = sc.nextInt();
+		}
+		for(int i = 0 ; i < a.length; i++)
+		{
+			ans[i] = a[k]; 
+			k--;
+		}
+		for(int i = 0 ; i < ans.length ; i++)
+		{
+			System.out.print(ans[i] + " ");
+		}
+	}
+}
