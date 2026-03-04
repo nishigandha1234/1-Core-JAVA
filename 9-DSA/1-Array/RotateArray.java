db97ddf77fe8ca1dd1f45fc187f2ed6d9ac61302
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-04 12:54:30 +0530

    New Array Programs Added Successfully...!

--- 9-DSA/1-Array/RotateArray.java
+++ 9-DSA/1-Array/RotateArray.java
@@ -9,7 +9,7 @@ class RotateArray
 		int a[] = new int[5];
 		int[] ans = new int[a.length];
 		int k = a.length;
-		for(int i = 0 ; i < a.length ; i++)
+		for(int i = 0 ; i < a.length-1 ; i++)
 		{
 			ans[i] = a[k]; 
 			k--;
