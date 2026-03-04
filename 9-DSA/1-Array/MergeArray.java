71cdf1e3d2cb59bfa73fd62147c29bd45e38e342
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-04 12:47:55 +0530

    New Array Programs Added Successfully...!

--- 9-DSA/1-Array/MergeArray.java
+++ 9-DSA/1-Array/MergeArray.java
@@ -2,4 +2,13 @@ import java.util.Scanner;
 
 class MergeArray
 {
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Values in a : ");
+		int a[] = new int[5];
+		System.out.println("Enter the Values in b : ");
+		int b[] = new int[5];
+		
+	}
 }
