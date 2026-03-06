d4fe34877b0e3adf65b48b840fd823efa6656b4e
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-06 12:42:40 +0530

    Commit

--- 9-DSA/1-Array/FindCommonElement.java
+++ 9-DSA/1-Array/FindCommonElement.java
@@ -24,7 +24,7 @@ class FindCommonElement
 			{
 				if(a[i] == b[j])
 				{
-					System.out.print("Common Elements are : "
+					System.out.print("Common Elements are : ");
 					System.out.print(b[j] + " ");
 				}
 			}
