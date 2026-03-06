b09666c4d3876dc1905ded22ba7405da9bc8b16d
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-06 12:30:34 +0530

    Commit

---
+++ 8-LeetcodeProblems/Happ Number.java
@@ -0,0 +1,6 @@
+//202. Happy Number
+
+//Starting with any positive integer, replace the number by the sum of the squares of its digits.Repeat the process
+
+class HappyNumber
+ 
---
+++ 8-LeetcodeProblems/HappyNumber.java
@@ -0,0 +1,19 @@
+//202. Happy Number
+
+//Starting with any positive integer, replace the number by the sum of the squares of its digits.Repeat the process
+
+class HappyNumber
+{
+	public static void main(String x[])
+	{
+		
+	}
+	public static void isHappy(int num)
+	{
+		for(int i = 1 ; i < num ; i++)
+		{
+			
+		}
+	}
+}
+ 
