27fbf6e00394f314616938a0f0f864f8fbbdbe5f
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-07 13:07:27 +0530

    New Added

--- 9-DSA/1-Array/InsertElementArray.java
+++ 9-DSA/1-Array/InsertElementArray.java
@@ -2,8 +2,12 @@ class InsertElementArray
 {
 	public static void main(String x[])
 	{
+		Scanner sc = new Scanner(System.in);
 		int a[] = {11,22,33,44,55};
-		int element = 777;
+		int b[] = new int[6];
+		int count = 5;
 		System.out.println("Enter the Element And Index : ");
+		int element = 777;
+		int k = 3;
 	}
 }
