f84fab041c00ec9afe3c0475ad49886b27fee87b
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-09 16:59:47 +0530

    New Files Addedd

--- 9-DSA/1-Array/EqualArray.java
+++ 9-DSA/1-Array/EqualArray.java
@@ -2,5 +2,9 @@ import java.util.Scanner;
 
 class EqualArray
 {
-	
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Values in an Array : ");
+	}
 }
