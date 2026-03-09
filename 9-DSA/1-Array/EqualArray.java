9d938d06b681f7794bfead4b0026e69d58b231af
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-09 17:02:42 +0530

    New Files Addedd

--- 9-DSA/1-Array/EqualArray.java
+++ 9-DSA/1-Array/EqualArray.java
@@ -5,6 +5,13 @@ class EqualArray
 	public static void main(String x[])
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Values in an Array : ");
+		System.out.println("Enter the Size and Values in First Array : ");
+		int size1 = sc.nextInt();
+		int a[] = new int[size1];
+		System.out.println("Enter the Size and Values in Second Array : ");
+		int size2 = sc.nextInt();
+		int b[] = new int[size2];
+		
+		for(int
 	}
 }
