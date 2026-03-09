2230175f9c258ecee5f68b85265e98077b1fecf5
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-09 17:42:00 +0530

    New Files Addedd

--- 9-DSA/1-Array/MissingElement.java
+++ 9-DSA/1-Array/MissingElement.java
@@ -4,7 +4,7 @@ class MissingElement
 {
 	public static void main(String x[])
 	{
-		Scanner sc = new Sacnner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Values in an Array : ");
 		int a[] = new int[5];
 		for(int i = 0 ; i < a.length ; i++)
---
+++ 9-DSA/1-Array/RotateArrayByNpositions.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class RotateArrayByNpositions
+{
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number : ");
+		int a[] = new int[5];
+		for(int i = 0 ; i<a.length ; i++)
+		{
+			a[i] = sc.nextInt();
+		}
+		
+	}
+}
