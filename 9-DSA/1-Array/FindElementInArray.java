5a1b818e7969db45af45a3d59212984972303892
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-09 16:38:43 +0530

    New Files Addedd

--- 9-DSA/1-Array/FindElementInArray.java
+++ 9-DSA/1-Array/FindElementInArray.java
@@ -7,6 +7,10 @@ class FindElementInArray
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter Values in Array : ");
 		int a[] = new int[5];
+		for(int i = 0 ; i < a.length ; i++)
+		{
+			a[i] = sc.nextInt();
+		}
 		
 	}
 }
