018c0ee55ad17f5337f465ee23d01778740dac38
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-09 16:30:46 +0530

    New Files Addedd

--- 9-DSA/1-Array/PrintGreaterNumber.java
+++ 9-DSA/1-Array/PrintGreaterNumber.java
@@ -2,4 +2,10 @@ import java.util.Scanner;
 
 class PrintGreaterNumber
 {
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Elements in an Array : ");
+		int[] a = 
+	}
 }
