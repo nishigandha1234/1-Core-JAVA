684dec14d95d2aa81b23323f6df9c9b95a27aae2
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-09 17:14:12 +0530

    New Files Addedd

--- 9-DSA/1-Array/MajorityElement.java
+++ 9-DSA/1-Array/MajorityElement.java
@@ -5,6 +5,11 @@ class MajorityElement
 	public static void main(String x[])
 	{
 		Scanner sc = new Scanner(System.in);
-		
+		System.out.println("Enter the Values in an Array : ");
+		int a[] = new int[5];
+		for(int i =0 ; i<a.length ; i++)
+		{
+			a[i] = sc.nextInt();
+		}
 	}
 }
