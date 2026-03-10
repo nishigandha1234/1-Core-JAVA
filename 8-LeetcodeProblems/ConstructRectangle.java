075a2ad4b407ba3f4d6e796415fe275db1456bb3
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-10 23:02:39 +0530

    New Files Added

--- 8-LeetcodeProblems/ConstructRectangle.java
+++ 8-LeetcodeProblems/ConstructRectangle.java
@@ -1,6 +1,7 @@
 //492. Construct the Rectangle
 //A web developer needs to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements
 
+import java.util.*;
 class ConstructRectangle
 {
 	public static void main(String x[])
