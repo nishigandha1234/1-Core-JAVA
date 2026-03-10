7c307539a9b4d69d674d69a346f0c6d5c533024c
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-10 23:02:52 +0530

    New Files Added

--- 8-LeetcodeProblems/ConstructRectangle.java
+++ 8-LeetcodeProblems/ConstructRectangle.java
@@ -6,7 +6,7 @@ class ConstructRectangle
 {
 	public static void main(String x[])
 	{
-		System.out.println(Arrays.toString(constructRectangle(37)));
+		System.out.println(Arrays.toString(constructRectangle(6)));
 	}
 	public static int[] constructRectangle(int area) 
 	{
