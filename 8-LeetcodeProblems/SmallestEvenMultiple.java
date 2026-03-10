0ade8dfa4fce7b323dd3cdc1374f7f17ff83ee34
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-10 19:59:31 +0530

    New Files Added

--- 8-LeetcodeProblems/SmallestEvenMultiple.java
+++ 8-LeetcodeProblems/SmallestEvenMultiple.java
@@ -9,10 +9,16 @@ class SmallestEvenMultiple
 	}
 	public static int smallestEvenMultiple(int n) 
 	{
-		for(int i = 2 ; i < n ; i++)
+		boolean flag = false;
+		int i ;
+		for(i = 2 ; i < n ; i++)
 		{
-			if(n%i == 0 && i%2 == 0)
-				return i;
+			if(n%i == 0 && i%2 == 0){
+				flag = true;
+				break;
+			}	
 		}
+		if(flag)
+			return i;
     }
 }
