0df9ecdd5b38454995d97634df9524c487b657cb
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-13 01:12:46 +0530

    String Programs Added

--- StringPractice/StringReverseApp.java
+++ StringPractice/StringReverseApp.java
@@ -6,6 +6,12 @@ class StringReverseApp
 	{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter String : ");
+		String str = sc.nextLine();
+		String word = "";
 		
+		for(int i = 0 ; i < str.length(); i++)
+		{
+			char ch = str.charAt(i);
+		}
 	}
 }
