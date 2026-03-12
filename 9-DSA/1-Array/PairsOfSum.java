35b0ddfd8b3e55b2da16231ec8b6ff13f1f55c01
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-12 13:19:58 +0530

    New Array Problems Done

--- 9-DSA/1-Array/PairsOfSum.java
+++ 9-DSA/1-Array/PairsOfSum.java
@@ -2,5 +2,17 @@ import java.util.Scanner;
 
 class PairsOfSum
 {
-
+	public static void main(String x[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Elements in Array : ");
+		int ar[] = new int[5];
+		for(int i = 0 ; i < ar.length ; i++)
+		{
+			ar[i] =sc.nextInt();
+		}
+		System.out.println("Enter the Target Element : ");
+		int target = sc.nextInt();
+		
+	}
 }
