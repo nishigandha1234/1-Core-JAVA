366ef4ffcc1d2f6f6041a8ebcc6da97df48da1b5
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-12 12:47:37 +0530

    New Array Problems Done

--- 9-DSA/1-Array/MoveZerosToLast.java
+++ 9-DSA/1-Array/MoveZerosToLast.java
@@ -6,14 +6,22 @@ class MoveZerosToLast
 	{
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Values in an Array : ");
-		int ar[] = new int[7];
+		int ar[] = new int[5];
 		for(int i = 0 ; i < ar.length ; i++)
 		{
 			ar[i] = sc.nextInt();
 		}
+		int index = 0;
 		for(int i = 0 ; i < ar.length ; i++)
 		{
-			
+			if(ar[i] != 0)
+			{
+				ar[index++] = ar[i];
+			}
 		}			
+		while(index < ar.length)
+		{
+			ar[index++] = 0;
+		}
 	}
 }
