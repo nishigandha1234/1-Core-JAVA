a65d337c2f90b6a541a073bfecf00adff943c94e
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-12 13:12:42 +0530

    New Array Problems Done

--- 8-LeetcodeProblems/MoveZerosToLast.java
+++ 8-LeetcodeProblems/MoveZerosToLast.java
@@ -7,6 +7,10 @@ class MoveZerosToLast
 	{
 		int[] nums = {0,1,0,3,12};
 		moveZeroes(nums);
+        for(int i = 0; i < nums.length; i++)
+        {
+            System.out.print(nums[i] + " ");
+        }
 	}
 	public static void moveZeroes(int[] nums) 
 	{
