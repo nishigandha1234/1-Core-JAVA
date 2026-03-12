748a51d01c153b430c2a125df8e73b7e4ea142d1
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-13 02:12:19 +0530

    String Programs Added

--- StringPractice/NonRepeatingDigit.java
+++ StringPractice/NonRepeatingDigit.java
@@ -3,10 +3,9 @@ class NonRepeatingDigit
 	public static void main(String x[])
 	{
 		String str = "Swiss";
-		int count = 0;
-		int i ;
-		for(i = 0 ; i < str.length(); i++)
+		for(int i = 0 ; i < str.length(); i++)
 		{
+			int count = 0;
 			for(int j = 0 ; j < str.length() ; j++)
 			{
 				if(str.charAt(i) == str.charAt(j))
