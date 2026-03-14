2f0538da7727e5f534ce165c590dba99fe318694
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-14 16:36:43 +0530

    Leetcode Done

---
+++ 8-LeetcodeProblems/MinimumCommon.java
@@ -0,0 +1,16 @@
+//2540. Minimum Common Value
+//Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. If there is no common integer amongst nums1 and nums2, return -1. Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
+
+class MinimumCommon
+{
+	public static void main(String x[])
+	{
+		int[] nums1 = {1,2,3};
+		int[] nums2 = {2,3};
+	}
+	public int getCommon(int[] nums1, int[] nums2) 
+	{
+        
+    }
+}
+}
