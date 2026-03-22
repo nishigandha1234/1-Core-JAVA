7f1ca801a11a1ad0bf9d0c6eebcb82fef3c0f824
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-22 15:52:51 +0530

    New File Added

--- 8-LeetcodeProblems/LuckyNumber.java
+++ 8-LeetcodeProblems/LuckyNumber.java
@@ -7,7 +7,7 @@ class LuckyNumber
 	{
 		int[] arr = {1,2,2,3,3,3};
 		System.out.println(findLucky(arr));
-		System.out.println(fin
+		System.out.println(fi
 	}
 	public static int findLucky(int[] arr)
 	{
