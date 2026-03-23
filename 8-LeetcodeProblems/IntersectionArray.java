4c522e2d8a45002eafc27592aa83a814bd5d6dad
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-24 00:02:16 +0530

    New Added

--- 8-LeetcodeProblems/IntersectionArray.java
+++ 8-LeetcodeProblems/IntersectionArray.java
@@ -5,6 +5,29 @@ class IntersectionArray
 {
 	public static void main(String x[])
 	{
-		
+		int[] nums1 = {1,2,2,3,4};
+		int[] nums2 = {1,2};	
+		System.out.println(intersection(nums1,nums2));
+	}
+	public static int[] intersection(int[] nums1, int[] nums2)
+	{
+		int[] ans = new int[nums1.length];
+		int k = 0 ;
+		for(int i = 0 ; i < nums1.length ; i++)
+		{
+			boolean flag = false;
+			for(int j = 0 ; j < nums2.length ; j++)
+			{
+				if(nums1[i] == nums2[j])
+				{
+					flag = true;
+					break;
+				}
+			}
+			if(!flag)
+			{
+				ans[k++] = nums1[i];
+			}
+		}
 	}
 }
