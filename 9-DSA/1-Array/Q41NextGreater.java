11f2099963019affad8b0402636e1f547c24dd14
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-23 19:04:08 +0530

    New Files

--- 9-DSA/1-Array/Q41NextGreater.java
+++ 9-DSA/1-Array/Q41NextGreater.java
@@ -14,7 +14,7 @@ class Q41NextGreater
                 }
             }
 
-            System.out.println("Next greater of " + a[i] + " is: " + nex
+            System.out.println("Next greater of " + a[i] + " is: " + n
             System.out.println("Next greater of " + a[i] + " is: " + next);
         }
     }
