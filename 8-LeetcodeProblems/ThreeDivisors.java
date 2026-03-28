607063764cd3219d849f9377b98dd399d601b98f
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-03-28 19:20:45 +0530

    New Files Added

--- 8-LeetcodeProblems/ThreeDivisors.java
+++ 8-LeetcodeProblems/ThreeDivisors.java
@@ -8,7 +8,7 @@ class ThreeDivisors
 		int n = 4;
 		int result = isThree(n);
 		System.out.println(result);
-		System.out.println(result)
+		System.out.println(resul
 	}
 	public static boolean isThree(int n) 
     {
