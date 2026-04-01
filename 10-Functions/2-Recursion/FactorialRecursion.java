f020e1ee37b71af8a5fdb18bb57a42299feae271
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-04-01 20:01:13 +0530

    New Files Added

---
+++ 10-Functions/2-Recursion/FactorialRecursion.java
@@ -0,0 +1,22 @@
+
+class FactorialRecursion
+{
+	static int fact =1 ;
+	public static void main(String x[])
+	{
+		fact(5);
+	}
+	public static void fact(int no)
+	{
+		
+		if(no != 0)
+		{
+			fact *= no;
+			no--;
+			fact(no);
+		}
+		else{
+			 System.out.println("Factorial  "+fact);
+		}
+	}
+}
