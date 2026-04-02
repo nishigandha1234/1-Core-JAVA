9cbb3d9e508396450a0760a4e5ccf990c0f4d8df
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-04-02 13:23:04 +0530

    New Files Added

--- 10-Functions/2-Recursion/FibonacciSeries.java
+++ 10-Functions/2-Recursion/FibonacciSeries.java
@@ -7,7 +7,7 @@ class FibonacciSeries
 		int limit = 5;
 		for(int i = 0 ; i <= limit ; i++)
 		{
-			System.out.println(fibo(n));
+			System.out.print(fibo(i) +" ");
 		}
 	}
 	public static int fibo(int n)
