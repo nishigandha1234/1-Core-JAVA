2620fed3ed720eb5f6101bff4c32918761f40767
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-04-03 22:22:52 +0530

    New Files Added

---
+++ 9-DSA/5-Functions/2-Simple_Functions_Assing/Q6SpecialNumber.java
@@ -0,0 +1,21 @@
+
+class Q6SpecialNumber
+{
+	public static void main(String x[])
+	{
+		int n = 12;
+		specialNumber(n);
+	}
+	public static void specialNumber(int n)
+	{
+		int sum = 0 , prod = 1;
+		int temp = n ;
+		while(temp != 0 )
+		{
+			int rem = temp % 10;
+			sum += rem;
+			prod *= rem;
+			temp /= 10;
+		}
+	}
+}
