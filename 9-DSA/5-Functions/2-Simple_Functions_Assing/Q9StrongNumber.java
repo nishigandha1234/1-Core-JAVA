6c351fec916fdfeb74f22625936d0eabc3e1d31f
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-04-03 22:44:07 +0530

    New Files Added

---
+++ 9-DSA/5-Functions/2-Simple_Functions_Assing/Q9StrongNumber.java
@@ -0,0 +1,16 @@
+
+class Q9StrongNumber
+{
+	public static void main(String x[])
+	{
+		int num = 145;
+		strongNumber(num);
+	}
+	public static void strongNumber(int num)
+	{
+		int temp = num;
+		while(temp != 0 )
+		{
+		}
+	}
+}
