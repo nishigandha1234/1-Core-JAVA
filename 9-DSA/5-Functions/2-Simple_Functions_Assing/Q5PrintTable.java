e15042f623309bbb596277c29318829991f652dc
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-04-03 21:09:39 +0530

    New Files Added

---
+++ 9-DSA/5-Functions/2-Simple_Functions_Assing/Q5PrintTable.java
@@ -0,0 +1,16 @@
+
+class Q5PrintTable
+{
+	public static void main(String x[])
+	{
+		int n = 5;
+		print(n);
+	}
+	public static void print(int n)
+	{
+		for(int i = 0 ; i <= 10 ; i++)
+		{
+			
+		}
+	}
+}
