65b10afb34f7ffd3a96bd794ca14074a8e9c0710
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-04-03 21:08:22 +0530

    New Files Added

--- 9-DSA/5-Functions/2-Simple_Functions_Assing/Q4PrintGreaterNumber.java
+++ 9-DSA/5-Functions/2-Simple_Functions_Assing/Q4PrintGreaterNumber.java
@@ -8,7 +8,7 @@ class Q4PrintGreaterNumber
 	}
 	public static void print(int n1, int n2)
 	{
-		if(n1 > n2 ) System.out.println(n1 + "is Greater");
-		else System.out.println(n2 + "is Greater");
+		if(n1 > n2 ) System.out.println(n1 + " is Greater");
+		else System.out.println(n2 + " is Greater");
 	}
 }
