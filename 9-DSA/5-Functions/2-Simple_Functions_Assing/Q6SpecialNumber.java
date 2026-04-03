fe40ebb6505adcabd87bf453b20afc85a527fc54
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-04-03 22:24:50 +0530

    New Files Added

--- 9-DSA/5-Functions/2-Simple_Functions_Assing/Q6SpecialNumber.java
+++ 9-DSA/5-Functions/2-Simple_Functions_Assing/Q6SpecialNumber.java
@@ -3,14 +3,14 @@ class Q6SpecialNumber
 {
 	public static void main(String x[])
 	{
-		int n = 12;
+		int n = 54;
 		specialNumber(n);
 	}
 	public static void specialNumber(int n)
 	{
 		int sum = 0 , prod = 1;
 		int temp = n ;
-		while(temp != 0 )
+		while(temp != 0)
 		{
 			int rem = temp % 10;
 			sum += rem;
