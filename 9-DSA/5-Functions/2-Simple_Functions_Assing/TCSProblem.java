a2315a77069440d6c729bf16f9cb091f2b943d8c
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-04-04 21:19:12 +0530

    New Files Added

--- 9-DSA/5-Functions/2-Simple_Functions_Assing/TCSProblem.java
+++ 9-DSA/5-Functions/2-Simple_Functions_Assing/TCSProblem.java
@@ -47,6 +47,6 @@ class TCSProblem
         }
 
         System.out.println("Most Frequent: " + maxElement);
-        System.out.println("Most Frequent:
+        System.out.println("Most Frequ
     }
 }
