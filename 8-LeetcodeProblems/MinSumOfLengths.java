39326b2052df1732ff4d8c358c5a88ee4a1249fb
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-04-19 21:57:54 +0530

    New Added

--- 8-LeetcodeProblems/MinSumOfLengths.java
+++ 8-LeetcodeProblems/MinSumOfLengths.java
@@ -45,7 +45,7 @@ class MinSumOfLengths
             }
         }
 
-        return ans == Integer.MAX_VALUE ? -1 : 
+        return ans == Integer.MAX_VALUE ? -1 :
         return ans == Integer.MAX_VALUE ? -1 : ans;
     }
 }
