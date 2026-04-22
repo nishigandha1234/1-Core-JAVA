2af9cba108827fef063d0e04f48416b7d99a5af1
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-04-23 00:26:41 +0530

    new String code added

--- 10-String/ThirdWayAnagramString.java
+++ 10-String/ThirdWayAnagramString.java
@@ -33,7 +33,7 @@ class ThirdWayAnagramString
 				}
 			}
 			if(flag) System.out.println("Anagram");
-			else System.out.println("NOT
+			else System.out.println("N
 			else System.out.println("NOT Anagram");
 		}
 	}
