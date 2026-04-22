bc20e7aa21f3cdfaf0479ed62d2b0bb0fe4de1b4
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-04-23 00:14:37 +0530

    new String code added

--- 10-String/AnagramStringWithoutBI.java
+++ 10-String/AnagramStringWithoutBI.java
@@ -49,7 +49,7 @@ class AnagramStringWithoutBI
 				}
 			}
 			if(flag) System.out.println("Anagram");
-			else System.out.println("NOT Ana
+			else System.out.println("NOT A
 			else System.out.println("NOT Anagram");
 		}
 	}
