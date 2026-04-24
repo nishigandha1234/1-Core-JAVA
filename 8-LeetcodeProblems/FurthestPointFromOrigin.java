576b07ee1ebefdb8c9bd44c676240b43c900bbb4
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-04-24 08:54:16 +0530

    New Array Problems Adde

---
+++ 8-LeetcodeProblems/FurthestPointFromOrigin.java
@@ -0,0 +1,15 @@
+//2833. Furthest Point From Origin
+//Input: moves = "L_RL__R" Output: 3 Explanation: The furthest point we can reach //from the origin 0 is point -3 through the following sequence of moves "LLRLLLR".
+
+class FurthestPointFromOrigin
+{
+	public static void main(String x[])
+	{
+		String moves = "L_RL__R";
+		
+	}
+	public static int furthestDistanceFromOrigin(String moves) 
+	{
+        
+    }
+}
