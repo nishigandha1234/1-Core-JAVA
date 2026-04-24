bb5286901957088622372896566dc3828e7f8a41
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-04-24 08:57:34 +0530

    New Array Problems Adde

--- 8-LeetcodeProblems/FurthestPointFromOrigin.java
+++ 8-LeetcodeProblems/FurthestPointFromOrigin.java
@@ -6,10 +6,28 @@ class FurthestPointFromOrigin
 	public static void main(String x[])
 	{
 		String moves = "L_RL__R";
-		
+		System.out.println(furthestDistanceFromOrigin(moves));
 	}
 	public static int furthestDistanceFromOrigin(String moves) 
 	{
-        
+        int lCount = 0 , rCount = 0, _Count = 0;
+		
+		for(int i = 0 ; i < moves.length() ; i++)
+		{
+			char ch = moves.charAt(i);
+			
+			if(ch == 'L')
+			{
+				lCount++;
+			}
+			else if(ch == 'R')
+			{
+				rCount++;
+			}
+			else{
+				_Count++;
+			}
+		}
+		int distance
     }
 }
