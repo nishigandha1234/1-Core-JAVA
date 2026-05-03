b8b3a1df26343f1de0315a31a0c0286bffc19dcb
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-05-04 00:08:21 +0530

    NEW STRING CODES ADDED

--- 8-LeetcodeProblems/WordBreak.java
+++ 8-LeetcodeProblems/WordBreak.java
@@ -16,7 +16,7 @@ class WordBreak
 	}
 	public static boolean wordBreak(String s, List<String> wordDict) 
 	{
-        boolean[] dp = new boolean[s.
+        boolean[] dp = new boolean[
         boolean[] dp = new boolean[s.length() + 1];
 		dp[0] = true;
 		for(int i = 1 ; i <= s.length() ; i++)
