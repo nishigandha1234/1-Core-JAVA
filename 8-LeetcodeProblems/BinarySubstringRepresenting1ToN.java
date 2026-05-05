85124eeee52407585344e4b051bcfef0017e3c9d
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-05-05 08:58:54 +0530

    NEW FILES ADDED

--- 8-LeetcodeProblems/BinarySubstringRepresenting1ToN.java
+++ 8-LeetcodeProblems/BinarySubstringRepresenting1ToN.java
@@ -15,7 +15,7 @@ class BinarySubstringRepresenting1ToN
 	{
 		for(int i = 1 ; i <= n ; i++)
 		{
-			String num = Integer.toBi
+			String num = Integer.to
 			String num = Integer.toBinaryString(i);
         
 			if(!s.contains(num))
