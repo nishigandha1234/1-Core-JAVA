86782423177acd29c96ae70ee598f4e612df9faf
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-05-09 09:05:30 +0530

    NEW FILES ADDED

--- 10-String/AnagramThirdLogic.java
+++ 10-String/AnagramThirdLogic.java
@@ -20,7 +20,7 @@ class AnagramThirdLogic
 			count[s2.charAt(i)]--;
 		}
 		boolean flag = true;
-		for(int i = 0 ; i < count
+		for(int i = 0 ; i < coun
 		for(int i = 0 ; i < count.length ; i++)
 		{
 			if(count[i] != 0)
