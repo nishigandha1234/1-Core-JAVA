075a004daa32e2745330b9fca4993983a1fdb52b
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-05-11 08:55:58 +0530

    NEW FILES ADDED

--- 8-LeetcodeProblems/MaxNumofJumpstoReachtheLastIndex.java
+++ 8-LeetcodeProblems/MaxNumofJumpstoReachtheLastIndex.java
@@ -9,7 +9,7 @@ class MaxNumofJumpstoReachtheLastIndex
 	{
 		int[] nums = {1,3,6,4,1,2};
 		int	target = 2;
-		System.out.println(maxim
+		System.out.println(
 		System.out.println(maximumJumps(nums,target));
 	}
 	public static int maximumJumps(int[] nums, int target) 
