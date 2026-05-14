30fd64dbd31ef2ead7fb37181aeb5fa73687bda1
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-05-14 08:45:52 +0530

    NEW FILES ADDED

--- 10-String/RotateString.java
+++ 10-String/RotateString.java
@@ -9,6 +9,7 @@ class RotateString
 		String s = "abcde";
 		String goal = "cdeab";
 		System.out.println(rotateString(s,goal));
+		System.out.println(rotateString(s,goal));
 	}
 	public static boolean rotateString(String s, String goal) 
 	{
