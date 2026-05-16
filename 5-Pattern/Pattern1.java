f309b21ff21be0feed472a1367764e4adf0fcc08
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-05-16 08:57:06 +0530

    NEW FILES ADDED

--- 5-Pattern/Pattern1.java
+++ 5-Pattern/Pattern1.java
@@ -5,7 +5,7 @@ public class Pattern1
 	public static void main(String x[])
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.println
+		System.out.pri
 		System.out.println("Enter the Number : ");
 		int n =sc.nextInt();
 		
