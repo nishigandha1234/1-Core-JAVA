45c15f045bb955f64ba64dc90a3c20a1ddc93f61
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-05-20 08:56:34 +0530

    NEW FILES ADDED

--- 10DaysStudyPlan/Topic1-ProgAssign/Day-7/ReturnArray124.java
+++ 10DaysStudyPlan/Topic1-ProgAssign/Day-7/ReturnArray124.java
@@ -13,7 +13,7 @@ class ReturnArray124
 	}
 	public static int[] getArray()
 	{
-		int[] a = new int[]{10,20,30,40,50
+		int[] a = new int[]{10,20,30,40,5
 		int[] a = new int[]{10,20,30,40,50};
 		return a;
 	}
