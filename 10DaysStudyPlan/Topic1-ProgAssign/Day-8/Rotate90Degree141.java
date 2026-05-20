a91f10ec917b56dfb0db5df71c5485f96e810eb2
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-05-20 09:16:48 +0530

    NEW FILES ADDED

---
+++ 10DaysStudyPlan/Topic1-ProgAssign/Day-8/Rotate90Degree141.java
@@ -0,0 +1,15 @@
+/*141 Rotate matrix 90 degrees LC 48 */
+
+class Rotate90Degree141
+{
+	public static void main(String x[])
+	{
+		
+		int[][] ar = new int[size][size];
+	}
+	public static int[] rotate90(int[][]ar)
+	{
+		
+		
+	}
+}
