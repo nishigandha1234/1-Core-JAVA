37bdb6eb38f94a513f62492c5306e1c9e2bcea65
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-05-24 11:48:21 +0530

    NEW FILES added

--- 10DaysStudyPlan/Topic1-ProgAssign/Day-9/StringCompression167.java
+++ 10DaysStudyPlan/Topic1-ProgAssign/Day-9/StringCompression167.java
@@ -8,7 +8,7 @@ class StringCompression167
     public static void main(String x[])
     {
         String s = "aaabbcccc";
-        System.out.println(
+        System.out.printl
         System.out.println(stringCompress(s));
     }
 
