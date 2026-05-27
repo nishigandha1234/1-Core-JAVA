8772d01cd75ed64b6484d9941bccfb67c32a833d
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-05-27 10:30:26 +0530

    NEW FILES ADDED

--- 12-OOPs/1-ClassesObjects/BankBalanceChecker.java
+++ 12-OOPs/1-ClassesObjects/BankBalanceChecker.java
@@ -16,7 +16,7 @@ class BankAccount
 	
 	void setInfo()
     {
-        System.out.
+        System.out
         System.out.print("Enter accountNumber : ");
         accountNumber = sc.nextInt();
 		
