be6238568f1ea35e7dd2515ba41e1939ef87ebba
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-05-28 10:31:29 +0530

    NEW FILES ADDED

--- 12-OOPs/1-ClassesObjects/ArrayComparison24.java
+++ 12-OOPs/1-ClassesObjects/ArrayComparison24.java
@@ -15,7 +15,7 @@ class ArrayCompare
 		{
 			sum1 += arr1[i];
 		}
-		System.out.println("Sum of Array
+		System.out.println("Sum of Arr
 		System.out.println("Sum of Array 1 : " + sum1);
 		
 		int sum2 = 0 ;
