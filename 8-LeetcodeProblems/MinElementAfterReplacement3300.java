861d450ef335b42ea6452a9d1e30140623a7bff3
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-05-29 19:53:59 +0530

    NEW FILES ADDED

--- 8-LeetcodeProblems/MinElementAfterReplacement3300.java
+++ 8-LeetcodeProblems/MinElementAfterReplacement3300.java
@@ -7,7 +7,7 @@ class MinElementAfterReplacement3300
 	public static void main(String x[])
 	{
 		int[] nums = {10,12,13,14};
-		Sy
+		S
 		System.out.println(minElement(nums));
 	}
 	public static int minElement(int[] nums) 
