edd84fd028cea65be7561bd4a0920a2073b61bfb
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-05-31 19:15:24 +0530

    NEW FILES ADDED

--- 8-LeetcodeProblems/MinElementAfterReplacement3300.java
+++ 8-LeetcodeProblems/MinElementAfterReplacement3300.java
@@ -6,11 +6,31 @@ class MinElementAfterReplacement3300
 {
 	public static void main(String x[])
 	{
-		int[] nums = {10,12,13,14};
+		int[] nums = {999,19,199};
 		System.out.println(minElement(nums));
+		//minElement(nums);
 	}
 	public static int minElement(int[] nums) 
 	{
-        
+		int[] ans = new int[nums.length];
+        for(int i = 0 ; i < nums.length ; i++)
+		{
+			int num = nums[i]; //10
+			int temp = num; 
+			int sum = 0;
+			while(temp != 0) //10
+			{
+				int rem = temp % 10;
+				sum += rem; 
+				temp /= 10;
+			}
+			ans[i] = sum;
+		}
+		int min = Integer.MAX_VALUE;
+		for(int i = 0 ; i < ans.length ; i++)
+		{
+			min = Math.min(ans[i], min);
+		}
+		return min;
     }
 }
---
+++ 8-LeetcodeProblems/SumofDigitsString1945.java
@@ -0,0 +1,20 @@
+/*1945. Sum of Digits of String After Convert
+You are given a string s consisting of lowercase English letters, and an integer k. Your task is to convert the string into an integer by a special process, and then transform it by summing its digits repeatedly k times. More specifically, perform the following steps:
+Convert: "zbax" ➝ "(26)(2)(1)(24)" ➝ "262124" ➝ 262124  Transform #1: 262124 ➝ 2 + 6 + 2 + 1 + 2 + 4 ➝ 17 Transform #2: 17 ➝ 1 + 7 ➝ 8
+Return the resulting integer after performing the operations described above.
+Example 1:Input: s = "iiii", k = 1  Output: 36*/
+
+class SumofDigitsString1945
+{
+	public static void main(String x[])
+	{
+		String s = "iiii";
+		int	k = 1;
+		System.out.println(minElement(nums));
+		//minElement(nums);
+	}
+	public static int getLucky(String s, int k) 
+	{
+        
+    }
+}
