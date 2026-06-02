93b6d49d89888ffc551901ee0e56623421c152ee
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-02 22:22:10 +0530

    NEW FILES ADDED

--- 8-LeetcodeProblems/SumofDigitsString1945.java
+++ 8-LeetcodeProblems/SumofDigitsString1945.java
@@ -21,7 +21,7 @@ class SumofDigitsString1945
 		{
 			ans += (s.charAt(i) - 'a' + 1);
 		}
-		for(int i = 
+		for(int i 
 		for(int i = 0 ; i < ans.length() ; i++)
 		{
 			sum += ans.charAt(i) - '0';
