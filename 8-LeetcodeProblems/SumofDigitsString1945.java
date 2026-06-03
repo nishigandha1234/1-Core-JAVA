3ed8aac3f5c3ea82a2388cbd2fed6df8aec63e78
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-03 23:37:03 +0530

    New Files Added

--- 8-LeetcodeProblems/SumofDigitsString1945.java
+++ 8-LeetcodeProblems/SumofDigitsString1945.java
@@ -10,7 +10,7 @@ class SumofDigitsString1945
 	{
 		String s = "iiii";
 		int	k = 1;
-		System.out.println(getLucky(s, k
+		System.out.println(getLucky(s, 
 		System.out.println(getLucky(s, k));
 	}
 	public static int getLucky(String s, int k) 
