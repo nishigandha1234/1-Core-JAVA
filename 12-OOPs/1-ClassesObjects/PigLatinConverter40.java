191a93a70105ad13d189d03629179e9c7558cc2b
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-06 12:14:27 +0530

    NEW FILES ADDED

--- 12-OOPs/1-ClassesObjects/PigLatinConverter40.java
+++ 12-OOPs/1-ClassesObjects/PigLatinConverter40.java
@@ -21,7 +21,7 @@ class PigLatinConverter
     String convertPigLatin()
     {
 		String ans = "";
-		for(int i =
+		for(int i
 		for(int i = 1 ; i < str.length() ; i++)
 		{
 			char ch = str.charAt(i);
