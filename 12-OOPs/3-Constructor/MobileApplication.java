b3cc551d81d50f3a2d3662d771b6550618905824
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-09 10:57:15 +0530

    CONSTRUCTOR QUESTIONS ADDED

--- 12-OOPs/3-Constructor/MobileApplication.java
+++ 12-OOPs/3-Constructor/MobileApplication.java
@@ -13,7 +13,7 @@ class Mobile
 	{
 		this.mobileId = mobileId;
 		this.mobileName = mobileName;
-		this.
+		thi
 		this.price = price - (price* 10/100);
 	}
 	Mobile(int mobileId, String mobileName, int price)
