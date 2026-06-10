9cfb95f990909d2978c99bc2ba4e17d210a240ac
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-10 12:28:53 +0530

    NEW FILES ADDED

--- 12-OOPs/3-Constructor/AccountApplication22.java
+++ 12-OOPs/3-Constructor/AccountApplication22.java
@@ -9,7 +9,7 @@ class Account
 	private long accountNumber;
 	private int balance;
 	
-	Account(long accountNumber, S
+	Account(long accountNumber, 
 	Account(long accountNumber, String name, int balance)
 	{
 		this.accountNumber = accountNumber;
