5b20867e512cf411ec71504556825da71c65ada5
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-13 14:50:43 +0530

    NEW FILES ADDED

--- 12-OOPs/3-Constructor/SingletonDriver.java
+++ 12-OOPs/3-Constructor/SingletonDriver.java
@@ -19,7 +19,7 @@ public class SingletonDriver
 {
 	public static void main(String x[])
 	{
-		Singleton 
+		Single
 		Singleton s1 = Singleton.getInstance();	
 		Singleton s2 = Singleton.getInstance();
 		
