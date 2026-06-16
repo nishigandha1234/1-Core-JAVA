64b409221b813ecc7ea8ce6d2979f7dade59f844
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-16 08:51:54 +0530

    NEW FILES ADDED"

--- 12-OOPs/5-StringProblemsOOP/PalindromeString10.java
+++ 12-OOPs/5-StringProblemsOOP/PalindromeString10.java
@@ -14,7 +14,7 @@ class PalindromeString10
 		if(word.equals(rev))
 			System.out.println("String is Palindrome ");
 		else
-			System.out.println(
+			System.out.pri
 			System.out.println("String is NOT Palindrome ");
 	}
 }
