c2f2b72f34303061a88f4e94fca73d3d55fbf517
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-17 12:14:28 +0530

    NEW FILES ADDED

--- 12-OOPs/4-ScenarioQuestOOP/EmployeeManagementSystem.java
+++ 12-OOPs/4-ScenarioQuestOOP/EmployeeManagementSystem.java
@@ -12,7 +12,7 @@ Input: Increase Rahul's salary.
 Expected: Only Rahul's salary changes.
 Test Case 3
 Input: Change company name. 
-Expected: All employees
+Expected: All employ
 Expected: All employees display the new company name.
 */
 
