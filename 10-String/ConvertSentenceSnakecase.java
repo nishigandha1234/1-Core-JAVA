3b3450ac30004c5976aebc2c0a942285c785b0c1
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-23 13:09:20 +0530

    NEW FILES ADDED

--- 10-String/ConvertSentenceSnakecase.java
+++ 10-String/ConvertSentenceSnakecase.java
@@ -6,7 +6,6 @@ class ConvertSentenceSnakecase
 	public static void main(String x[])
 	{
 		Scanner sc = new Scanner(System.in);
-		System.ou
 		System.out.println("Enter the String : ");
 		String s = sc.nextLine(); 
 		String ans = "";
