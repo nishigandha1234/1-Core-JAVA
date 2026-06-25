c3f8fcb370f913023fce55a13465fb6987c4cc2e
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-25 11:38:14 +0530

    NEW FILES ADDED

--- 10-String/MinDeletionsPalindrome.java
+++ 10-String/MinDeletionsPalindrome.java
@@ -12,7 +12,7 @@ class MinDeletionsPalindrome
 
         while(left < right)
         {
-            if(s.charAt(left) == s.charAt(r
+            if(s.charAt(left) == s.charA
             if(s.charAt(left) == s.charAt(right))
             {
                 left++;
