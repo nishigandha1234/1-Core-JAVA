93de4c7d2422d504f3202bdeb2aefc53489842ea
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-26 13:10:33 +0530

    NEW FILES ADDED

--- 8-LeetcodeProblems/countMajoritySubarrays.java
+++ 8-LeetcodeProblems/countMajoritySubarrays.java
@@ -13,7 +13,7 @@ class countMajoritySubarrays
 	public static int countMajoritySubarrays(int[] nums, int target) 
 	{
 		int ans = 0;
-		for(int i = 0 
+		for(int 
 		for(int i = 0 ; i < nums.length ; i++)
 		{
 			int targetCount = 0 ;
