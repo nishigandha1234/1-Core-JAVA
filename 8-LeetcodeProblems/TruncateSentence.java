d787c5e3dd91fc73f2dee586510e7d120d1cfa39
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-27 12:37:46 +0530

    NEW FILES ADDED

--- 8-LeetcodeProblems/TruncateSentence.java
+++ 8-LeetcodeProblems/TruncateSentence.java
@@ -8,7 +8,6 @@ class TruncateSentence
 	public static void main(String x[])
 	{
 		String s = "Hello how are you Contestant";
-		int
 		int	k = 4;
 		System.out.println(truncateSentence(s,k));		
 	}
