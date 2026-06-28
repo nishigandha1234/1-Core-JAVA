b552f26a0ef48bf7b42b6d2ca65432403bf37593
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-28 15:14:50 +0530

    NEW FILES ADDED

---
+++ 8-LeetcodeProblems/MaxProductDiff.java
@@ -0,0 +1,41 @@
+/*1913. Maximum Product Difference Between Two Pairs
+The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.Return the maximum such product difference.
+Input: nums = [5,6,2,7,4] Output: 34*/
+
+class MaxProductDiff
+{
+	public static void main(String x[])
+	{	
+		int[] nums = {5,6,2,7,4};
+		System.out.println(maxProductDifference(nums));
+	}
+	public static int maxProductDifference(int[] nums) 
+	{
+        int max = Integer.MIN_VALUE;
+		int	secMax = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+		int secMin = Integer.MAX_VALUE;
+		
+		for(int i = 0 ; i < nums.length ; i++)
+		{
+			if(nums[i] > max)
+			{
+				secMax = max;
+				max = nums[i];
+			}
+			else if(nums[i] > secMax)
+				secMax = nums[i];			
+		}
+		for(int i = 0 ; i < nums.length ; i++)
+		{
+			if(nums[i] < min)
+			{
+				secMin = min;
+				min = nums[i];
+			}
+			else if(nums[i] < secMin)
+				secMin = nums[i];			
+		}
+		return (max*secMax)-(min*secMin);
+    }
+}
---
+++ 8-LeetcodeProblems/SmallestIndex3550.java
@@ -0,0 +1,32 @@
+/*3550. Smallest Index With Digit Sum Equal to Index
+You are given an integer array nums.Return the smallest index i such that the sum of the digits of nums[i] is equal to i.If no such index exists, return -1.
+Example 1:Input: nums = [1,3,2] Output: 2*/
+
+class SmallestIndex3550
+{
+	public static void main(String x[])
+	{
+		int[] nums = {1,3,2};
+		System.out.println(smallestIndex(nums));
+	}
+	public static int smallestIndex(int[] nums) 
+	{
+		
+        for(int i = 0 ; i < nums.length ; i++)
+		{
+			int temp = nums[i];
+			int sum  = 0;
+			while(temp != 0)
+			{
+				int rem = temp % 10;
+				sum += rem;
+				temp /= 10;
+			}
+			if(sum == i)
+			{
+				return i;
+			}
+		}
+		return -1;
+    }
+}
