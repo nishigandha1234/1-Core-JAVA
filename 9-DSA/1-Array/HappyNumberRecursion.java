f1b175faab25d8ada5786fc064c7651119ffee5e
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-06-30 22:45:01 +0530

    NEW FILES ADDED

--- 9-DSA/1-Array/HappyNumberRecursion.java
+++ 9-DSA/1-Array/HappyNumberRecursion.java
@@ -5,6 +5,7 @@ class HappyNumberRecursion
 	public static void main(String x[])
 	{
 		int num = 19;
+		System.out.println(isHappy(num
 		System.out.println(isHappy(num));
 	}
 	public static boolean isHappy(int num)
