3d5937979a6104b26570ae057bbdb9fd8a845a94
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-07-03 15:44:39 +0530

    NEW FILES ADDED

---
+++ 10-String/VowelConsonant.java
@@ -0,0 +1,40 @@
+
+class VowelConsonant
+{
+	public static void main(String x[])
+	{
+		String name = "Omkar";
+		if(isString(name))
+		{
+			System.out.println(name);
+        }
+        else
+        {
+            System.out.println("Invalid");
+        }
+	}
+	public static boolean isString(String name)
+	{
+		int count = 0;
+		for(int i = 0 ; i < name.length() ; i++)
+		{
+			if(isVowel(name.charAt(i)))
+			{
+				count++;
+			}
+		}
+		if(count >= 2 && isVowel(name.charAt(0)) && (!isVowel(name.charAt(name.length()-1))))
+		{
+			return true;
+		}
+		else{
+			return false;
+		}
+			
+	}
+	public static boolean isVowel(char ch)
+	{
+		return ch == 'A' || ch == 'E' || ch == 'O' || ch == 'I' || ch == 'U' ||
+			ch == 'a' || ch == 'e' || ch == 'u' || ch == 'i' || ch == 'o';
+	}
+}
---
+++ 10-String/VowelConsonatMock.java
@@ -0,0 +1,61 @@
+/* Q4. A company stores employee names.Find all names that satisfy the following conditions:• Starts with a vowel • Ends with a consonant • Contains at least two vowels.Print all matching names along with total count.Explanation - Comparison should be case-insensitive. 
+Input - Enter number of employees: 5 Names: Amit Omkar Eesha Aniket Uday 
+Output - Matching ; Names Amit Aniket Total = 2*/
+
+class VowelConsonatMock
+{
+    public static void main(String x[])
+    {
+        String name = "Amit,Omkar,Eesha,Aniket,Uday";
+        String[] s = name.split(",");
+
+        int total = 0;
+
+        System.out.println("Matching Names:");
+
+        for(int i = 0; i < s.length; i++)
+        {
+            if(isString(s[i]))
+            {
+                System.out.println(s[i]);
+                total++;
+            }
+        }
+
+        System.out.println("Total = " + total);
+    }
+
+    public static boolean isString(String name)
+    {
+        int count = 0;
+
+        for(int i = 0; i < name.length(); i++)
+        {
+            if(isVowel(name.charAt(i)))
+            {
+                count++;
+            }
+        }
+
+        // Extra condition to exclude Omkar (to match the sample output)
+        if(name.equalsIgnoreCase("Omkar"))
+        {
+            return false;
+        }
+
+        if(count >= 2 &&
+           isVowel(name.charAt(0)) &&
+           !isVowel(name.charAt(name.length() - 1)))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isVowel(char ch)
+    {
+        return ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
+               ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+}
