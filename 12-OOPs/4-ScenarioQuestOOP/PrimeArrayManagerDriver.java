99b7ab619cecf7d530625144a29478d839057675
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-07-08 14:38:48 +0530

    NEW FILES ADDED

--- 12-OOPs/4-ScenarioQuestOOP/PrimeArrayManagerDriver.java
+++ 12-OOPs/4-ScenarioQuestOOP/PrimeArrayManagerDriver.java
@@ -10,7 +10,7 @@ class PrimeArrayManager {
     public PrimeArrayManager(int[] arr) {
         this.arr = arr;
         prime = new int[arr.length];
-        noPrime = new in
+        noPrime = new
         noPrime = new int[arr.length];
     }
 
