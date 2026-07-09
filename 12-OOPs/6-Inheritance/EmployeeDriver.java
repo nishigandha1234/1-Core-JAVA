1aa4fe881a37811a5b44205c5dab05310cb58cf0
nishigandha1234 <nishigandhakakade21004@gmail.com>
2026-07-09 12:32:18 +0530

    NEW FILES ADDED

--- 12-OOPs/6-Inheritance/EmployeeDriver.java
+++ 12-OOPs/6-Inheritance/EmployeeDriver.java
@@ -4,7 +4,7 @@ Output Employee Name: Vivek Salary: 30000*/
 
 abstract class Employee
 {
-	abstract void c
+	abstract vo
 	abstract void calculateSalary();
 } 
 class FullTimeEmployee extends Employee
